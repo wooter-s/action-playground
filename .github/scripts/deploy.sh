911a224d75ff0954808c2b27d0f31e2975e3b6f8
shenyicheng <shenyicheng@souche.com>
2020-02-17 10:06:18 +0800

    添加访问权限

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,5 +2,5 @@
 echo "HOST is ${HOST}"
 echo "${SECRET}" > "${FILENAME}"
 chmod 600 "${FILENAME}"
-rm -f ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
 scp -i "${FILENAME}" test.ts "${USER}@${HOST}:/"
